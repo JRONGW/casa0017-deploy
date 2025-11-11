f3c7ac0b8844ee54e7019bcc8b39f1eb36346418
Junrong Wang <jrongwchn@gmail.com>
2025-11-11 07:33:13 +0000

    Update app.js

--- Website/server/routes/app.js
+++ Website/server/routes/app.js
@@ -148,9 +148,6 @@ app.get(/.*\.html$/, (req, res) => {
   res.sendFile(path.join(CLIENT_DIR, req.path.replace(/^\//, "")));
 });
 
-app.use("/js",  express.static(path.join(root, "Website/client/js")));
-app.use("/css", express.static(path.join(root, "Website/client/css")));
-app.use("/img", express.static(path.join(root, "Website/client/img")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(CLIENT_DIR, "index.html"));
